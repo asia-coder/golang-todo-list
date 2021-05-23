5a172ff15bde4e735af47a87afc4b8cd1ce63ea4
Dilshod Muydinov <uktamovich95@gmail.com>
2021-05-23 18:44:57 +0500

    created migrations for DB added env variables connection to DB

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,53 @@ package main
 import (
 	"github.com/asiaCoder/todo-app"
 	"github.com/asiaCoder/todo-app/pkg/handler"
+	"github.com/asiaCoder/todo-app/pkg/repository"
+	"github.com/asiaCoder/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Ошибка при чтение конфигурационного файла: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Ошибка при чтение переменных окружения: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.ssl_mode"),
+	})
+
+	if err != nil {
+		log.Fatalf("Ошибка при инициализации БД: %s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	err := srv.Run("8080", handlers.InitRoutes())
+	err = srv.Run(viper.GetString("http.port"), handlers.InitRoutes())
 
 	if err != nil {
 		log.Fatalf("Ошибка при работе http сервера: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
